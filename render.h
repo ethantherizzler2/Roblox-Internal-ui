9918fc2cdd87f366c178d9d71ae7fd3faa68d771
Ethan <ethandiddy33@gmail.com>
2026-02-12 19:09:34 +0100

    Clean up render.h by removing unused variables
    Removed unnecessary render variables from render.h

--- render.h
+++ render.h
@@ -9,17 +9,6 @@
 
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-ID3D11Device* rbx::render::device = nullptr;
-ID3D11DeviceContext* rbx::render::device_context = nullptr;
-IDXGISwapChain* rbx::render::swap_chain = nullptr;
-ID3D11RenderTargetView* rbx::render::render_target_view = nullptr;
-rbx::render::present_fn rbx::render::original_present = nullptr;
-rbx::render::resize_buffers_fn rbx::render::original_resize_buffers = nullptr;
-WNDPROC rbx::render::original_wnd_proc = nullptr;
-HWND rbx::render::our_window = nullptr;
-bool rbx::render::is_init = false, rbx::render::draw = false;
-float rbx::render::dpi_scale = 1.f;
-
 namespace rbx {
     class render {
     public:
