29f9da4291858fee6c40ef7b0a64701e33aa81f9
Ethan <ethandiddy33@gmail.com>
2026-02-12 19:13:07 +0100

    Add job-related constants to Scripts namespace

--- offsets.hpp
+++ offsets.hpp
@@ -9,6 +9,13 @@ namespace Offsets
     const uintptr_t RawScheduler = REBASE(0x7D33708);
     const uintptr_t Print = REBASE(0x1737F90);
 
+    namespace Scripts {
+        const uintptr_t JobEnd = 0x1D8;
+        const uintptr_t JobId = 0x138;
+        const uintptr_t JobStart = 0x1D0;
+        const uintptr_t Job_Name = 0x18;
+        const uintptr_t JobsPointer = 0x7E4EEE0; // Not needed But is usefull!
+    }
     namespace render {
         const uintptr_t renderview = 0x218; // most of the time the same
         const uintptr_t DeviceD3D11 = 0x8; // same
